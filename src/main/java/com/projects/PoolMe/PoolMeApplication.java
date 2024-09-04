f8e5d3b35863ab705d42297f26ed14f9b32dc5ab
Srinivas Hapnoor <hsrinivas185@gmail.com>
2024-09-03 20:52:38 -0500

    adding spring

---
+++ src/main/java/com/projects/PoolMe/PoolMeApplication.java
@@ -0,0 +1,13 @@
+package com.projects.PoolMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/PoolMe/PoolMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.PoolMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
